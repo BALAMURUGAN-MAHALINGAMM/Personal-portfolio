fb73e9c4686f2a1ca1e8b30780b17a3836f01556
BALAMURUGAN-MAHALINGAMM <balamuruganmaha2003@gmail.com>
2024-06-13 16:53:44 +0530

    this is my portfolio

---
+++ assests/script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu = document.querySelector(".navbar-links");
+    const icons = document.querySelector(".navbar-icons");
+    menu.classList.toggle('open');
+    icons.classList.toggle('open');
+}
